3091a48835b8393dd4f4b1ac56ebe4d9f35377c9
GAUREN <garethyoo@gmail.com>
2025-06-26 09:41:09 +0200

    first commit

---
+++ src/main/java/com/yootiful/animal_shelter_assistant/AnimalShelterAssistantApplication.java
@@ -0,0 +1,14 @@
+package com.yootiful.animal_shelter_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalShelterAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalShelterAssistantApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/yootiful/animal_shelter_assistant/AnimalShelterAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yootiful.animal_shelter_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelterAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
