f453cf83a78fd7b71176b769d854c90acb3ac597
GAUREN <garethyoo@gmail.com>
2025-06-27 07:49:14 +0200

    add chat with advisor to remember previous requests

--- src/main/java/com/yootiful/animal_shelter_assistant/AnimalShelterAssistantApplication.java
+++ src/main/java/com/yootiful/animal_shelter_assistant/AnimalShelterAssistantApplication.java
@@ -1,7 +1,22 @@
 package com.yootiful.animal_shelter_assistant;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 
 @SpringBootApplication
 public class AnimalShelterAssistantApplication {
@@ -10,5 +25,35 @@ public class AnimalShelterAssistantApplication {
 		SpringApplication.run(AnimalShelterAssistantApplication.class, args);
 	}
 
+	@Controller
+	@ResponseBody
+	class AnimalShelterAssistantController {
+
+		private final ChatClient ai;
+		// each user has it's own memory with concurrent hashmap
+		private final Map<String, PromptChatMemoryAdvisor> memory = new ConcurrentHashMap<>();
+
+        AnimalShelterAssistantController(ChatClient.Builder ai) {
+            this.ai = ai.build();
+        }
+
+        @GetMapping("/{user}/assistant")
+		String assistant (@PathVariable String user, @RequestParam String question) {
+
+			// add an advisor to allow the assistant to have memory, this acts as a filter to pre-process the request
+			var advisor = PromptChatMemoryAdvisor
+					.builder(
+							MessageWindowChatMemory.builder().chatMemoryRepository(new InMemoryChatMemoryRepository()).build()
+					)
+					.build();
+			var advisorForUser = this.memory.computeIfAbsent(user,  u -> advisor);
+			return this.ai
+					.prompt()
+					.user(question)
+					.advisors(advisorForUser)
+					.call()
+					.content();
+		}
+	}
 
 }
